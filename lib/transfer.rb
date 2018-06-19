96ecfb013e6b1956847209d0e454d928ff5acb96
Gabriel Valle <gvalle91@gmail.com>
2018-06-19 03:47:20 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,7 +30,7 @@ class Transfer
     if self.status = "complete"
       self.sender.balance += self.amount
       self.receiver.balance -= self.amount
-      self.status = "pending"
+      self.status = "reversed"
     end
   end
   
