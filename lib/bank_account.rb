688be23fe2208f4989d9217a738b8dd633b57047
Gabriel Valle <gvalle91@gmail.com>
2018-06-19 03:25:41 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,13 @@
 class BankAccount
-  
+  attr_reader :balance, :status
   
   def initialize (name)
     @name = name
     @balance = 1000
     @status = "open"
+  end
+  
+  def deposit (amount)
+    
   end
 end
