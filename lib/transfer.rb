82c0f9a64f54f461c113f330aa84534e99549cde
Gabriel Valle <gvalle91@gmail.com>
2018-06-19 03:45:08 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,11 +12,11 @@ class Transfer
   end
   
   def valid?
-    self.sender.valid? && self.sender.balance >= self.amount && self.receiver.valid?
+    self.sender.valid?  && self.receiver.valid?
   end
   
   def execute_transaction
-    if self.valid? && self.status == "pending"
+    if self.valid? && self.sender.balance >= self.amount && self.status == "pending"
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       self.status = "complete"
